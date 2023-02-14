5b229933be200e0d17588ea2eddd343905ae45ea
CaribouW <lfm1012@sjtu.edu.cn>
2023-02-14 13:20:32 +0800

    add id_deref builtin function for helping

--- sample.py
+++ sample.py
@@ -17,6 +17,11 @@ print(id(di), hex(id(di)))
 print(id(di['4']), hex(id(di['4'])))
 print(id(tmp), hex(id(tmp)))
 print(id(int1), hex(id(int1)))
+
+t = id(li)
+print(id_deref(t))
+print(id_deref(id(li)))
+
 # BASE_HEX=4ffff5a00000 TOTAL_SZ_HEX=40000000 LD_PRELOAD=/home/lfm/libmalloc_wrapper.so ./python sample.py
 
 
